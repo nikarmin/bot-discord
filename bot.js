42e626660fb07d2c5a5a038bf71c0231cf464268
Nicoli <nikafworks@gmail.com>
2021-10-28 21:25:11 -0300

    !sabedoria

--- bot.js
+++ bot.js
@@ -4,15 +4,11 @@ require('dotenv').config();
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({intents});
 const config = require('./config.json');
-
-client
+const handlers = require("./node_modules/discord.js/src/client/websocket/handlers");
 
 client.on("ready", () =>{
     console.log(`Bot iniciado!`);
-});
-
-client.on("guildCreate", guild => {
-    client.user.setActivity(`Estou em ${client.guilds.size}`);
+    client.user.setStatus("raspando cabelos");
 });
 
 client.on("message", async message =>{
@@ -23,12 +19,39 @@ client.on("message", async message =>{
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
 
+    client.user.setStatus({})
+
+    //grandes comandos com nomes brasileiros
     if(comando === "adryan" || comando === "nicoli" || comando === "camila"){
         await message.channel.send("CARECA!");
     }
     
+    //messi careca 
     if(comando === "careca"){
         await message.channel.send({ files : ["https://pbs.twimg.com/profile_images/1289324739794735104/mQoHcZfq_400x400.jpg"]});
+    }
+
+    //frase para os carecas de plantão
+    if(comando === "frase"){
+        await message.channel.send({ files : ["https://cdn.pensador.com/img/frase/we/sl/wesley_d_amico_as_vezes_esqueco_que_sou_careca_e_me_vej_l5eyreq.jpg"]});
+    }
+
+    //perguntas tops com um sistema excepcional de switch
+    if(comando === "sabedoria"){
+        const random = Math.floor(Math.random() * 3);
+        switch(random){
+            case 0:
+                await message.channel.send("sim :)");
+                break;
+
+            case 1:
+                await message.channel.send("talvez :|");
+                break;
+
+            case 2:
+                await message.channel.send("não kkkkkkkkkkkkkk :D");
+                break;
+        }
     }
 });
 
